14cdc557734d643758cee15826e3a33e3e9a3894
Mark <markmal64@gmail.com>
2026-02-18 19:17:57 +1100

    refactor: remove explicit wildcard path from catch-all `app.use` middleware definitions.

--- server/static.ts
+++ server/static.ts
@@ -16,7 +16,7 @@ export function serveStatic(app: Express) {
   // app.use('/attached_assets', express.static(path.resolve(__dirname, '../../attached_assets')));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
